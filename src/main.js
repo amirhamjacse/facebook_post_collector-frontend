e86cd6c478e9e2a1856c874279214e6fa541b81f
amirhamjacse <amirhamza824@gmail.com>
2025-11-26 12:34:16 +0600

    changes some

--- src/main.js
+++ src/main.js
@@ -55,7 +55,7 @@ mainStore.fetchSampleHistory()
 // }
 
 // Default title tag
-const defaultDocumentTitle = 'Admin One Vue 3 Tailwind'
+const defaultDocumentTitle = 'Facebook Crawler Admin'
 
 // Set document title from route meta
 router.afterEach((to) => {
